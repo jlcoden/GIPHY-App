995f60db0596a1564383fe8940c54f2d078a298d
Josh Cosson <joshlcosson@gmail.com>
2019-09-29 01:18:15 -0700

    Added a for loop in app.js so that a total of 10 images are appended per click of the button, also added first version of styles.css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,55 +3,50 @@ var topics = ["Cow", "Sheep", "Moose", "Lion"];
   //document wrapper 
   $(document).ready(function() {
 
-// displaytopicInfo function re-renders the HTML to display the appropriate content
+// displaytopicInfo function re-renders the HTML to display the  content
 function displayTopicInfo() {
 
+  //parameters that make up the queryURL
   var topic = $(this).attr("data-name");
-  
   var api = 'https://api.giphy.com/v1/gifs/search?';
-
-  var limit = '&limit=25';
-
+  var limit = '&limit=10';
   var key = 'api_key=ZX2nFAngcwSZ3yPNLC9dFOA7jWxaU0X1&q=';
-
   var rating = '&rating=G';
 
+  //queryURL variable
   var queryURL = api + key + topic + limit + rating;
 
 
   console.log(topic); 
-
   console.log(queryURL); 
 
-
-  // Creating an AJAX call for the specific topic button being clicked
+  // AJAX call for the specific for the topic button being clicked
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
 
-        console.log(response); 
+    var results = response.data; 
 
+   for (var i = 0; i < results.length; i++) {
 
 //     // Creating a div to hold the topic
      var topicDiv = $("<div class='topic'>");
 
 //     // Storing the rating data
-      var rating = response.data[0].rating;
+      var rating = results[i].rating;
 
 //     // Creating an element to have the rating displayed
       var pOne = $("<p>").text("Rating: " + rating);
-
+    
+      //console.log rating
       console.log(rating); 
 
 //     // Displaying the rating
        topicDiv.append(pOne);
 
 //     // Retrieving the URL for the image
-       var imgURL = response.data[0].url;
-
-       console.log(imgURL); 
-  
+       var imgURL = results[i].images.fixed_height.url;
 
 //     // Creating an element to hold the image
        var image = $("<img>");
@@ -60,35 +55,36 @@ function displayTopicInfo() {
        image.attr("src", imgURL);
        image.attr("alt", "topic image");
        
-//     // Appending the image
+      // Appending the image
       topicDiv.append(image);
 
-//     // Putting the entire topic above the previous topics
+     // Putting the entire topic above the previous topics
      $("#topics-view").prepend(topicDiv);
+    }
    });
 
  }
 
-// // Function for displaying topic data
+ // Function for displaying topic data
  function renderButtons() {
 
-//   // Deleting the topics prior to adding new topics
-//   // (this is necessary otherwise you will have repeat buttons)
+   // Deleting the topics prior to adding new topics
+   // (this is necessary otherwise you will have repeat buttons)
    $("#buttons-view").empty();
 
 //   // Looping through the array of topics
    for (var i = 0; i < topics.length; i++) {
 
-//     // Then dynamicaly generating buttons for each topic in the array
-//     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+     // Then dynamicaly generating buttons for each topic in the array
+     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
      var a = $("<button>");
-//     // Adding a class of topic-btn to our button
+     // Adding a class of topic-btn to our button
      a.addClass("topic-btn");
-//     // Adding a data-attribute
+    // Adding a data-attribute
      a.attr("data-name", topics[i]);
-//     // Providing the initial button text
+     // Providing the initial button text
      a.text(topics[i]);
-//     // Adding the button to the buttons-view div
+     // Adding the button to the buttons-view div
      $("#buttons-view").append(a);
    }
 }
